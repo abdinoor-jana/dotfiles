99aed2f816c694ee3b930dd65850d6c668822af0
Dan Abdinoor <abdinoor@jana.com>
2015-03-19 11:08:48 -0400

    turn off share history. update jana tools directory

--- system/jana.zsh
+++ system/jana.zsh
@@ -1,4 +1,4 @@
-export PATH=$PATH:~/jana/tools
+export PATH=$PATH:~/jana/experiments/bin
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home"
@@ -8,3 +8,5 @@ source /usr/local/bin/virtualenvwrapper.sh
 alias jana="source jana_activate"
 alias kafka="~/jana/kafka-0.7.2-incubating-src/bin/kafka-server-start.sh ~/jana/kafka-0.7.2-incubating-src/config/server.properties"
 alias zookeeper="~/jana/kafka-0.7.2-incubating-src/bin/zookeeper-server-start.sh ~/jana/kafka-0.7.2-incubating-src/config/zookeeper.properties"
+
+export JANAENV=local
