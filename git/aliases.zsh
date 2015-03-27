ee205e23684c6a60fe361f6400b112fa3e2bfd66
Dan Abdinoor <abdinoor@jana.com>
2015-03-27 11:10:56 -0400

    updating all manner of git things

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,17 +7,13 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gd='git diff'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
+alias gch='git-commit-cli && git push'
+alias gc='git-commit-cli'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gp='git pull'
+alias gd='git diff --histogram'
 alias gh='git push'
 alias ga='git add --all'
-alias gc='~/scripts/git-commit.sh'
-alias gch='~/scripts/git-commit.sh && git push'
+alias gb='git branch'
 alias gk='git checkout'
